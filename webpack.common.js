d92579b30128872af932813bb00edcfcd5859ec8
Pandiarajan R <pandiarajan.rajagopal@nutechnologyinc.com>
2021-11-08 17:28:45 -0600

    file and html loader added

--- webpack.common.js
+++ webpack.common.js
@@ -1,30 +1,41 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader", //3. Injects style tag in to DOM
-                    "css-loader", //2. Turns css into commonjs
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader", //3. Injects style tag in to DOM
-                    "css-loader", //2. Turns css into commonjs
-                    "sass-loader" //1. Turns scss into css
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/template.html'
-        })
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", //3. Injects style tag in to DOM
+          "css-loader", //2. Turns css into commonjs
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Injects style tag in to DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader", //1. Turns scss into css
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        loader: "file-loader",
+        options: {
+          name: '[name].[contenthash].[ext]',
+          outputPath: '/imgs'
+        }
+      }
     ],
-}
+  }
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -8,5 +9,8 @@ module.exports = merge(common, {
     output: {
         filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
 });
