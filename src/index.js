f5f09d6fa0a7b5c4a6b84077d408bc2ec3589668
Pandiarajan R <pandiarajan.rajagopal@nutechnologyinc.com>
2021-11-07 21:45:30 -0600

    webpack bundle of helper and single main.js file added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('hello from webpack')
+import { AlertService } from './app/alert.service';
+import { run } from './app/app';
+import { ComponentService } from './app/component.service';
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+run(alertService, componentService);
